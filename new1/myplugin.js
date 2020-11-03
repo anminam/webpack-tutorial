d764c92d3f6a36ea5b321b500ee7e92f6962e7c8
anminam <minamhunmin@gmail.com>
2020-11-03 17:06:28 +0900

    update for plugin

--- new1/myplugin.js
+++ new1/myplugin.js
@@ -4,12 +4,31 @@ class MyPlugin {
     //   console.log("MyPlugin: done");
     // });
 
+    compiler.hooks.emit.tapAsync(
+      "이이름이 어서보이냐",
+      (compilation, callback) => {
+        const source = compilation.assets["main.js"].source();
+        compilation.assets["main.js"] = {
+          source: () => {
+            const banner = [
+              "/**",
+              " * 이것은 BannerPlugin이 처리한 결과입니다.",
+              " * Build Date: 2019-10-10",
+              " */",
+            ].join("\n");
+            return banner + "\n" + source;
+          },
+        };
+        callback();
+      }
+    );
+
     // compiler.plugin() 함수로 후처리한다
-    compiler.plugin("emit", (compilation, callback) => {
-      const source = compilation.assets["main.js"].source();
-      console.log(source);
-      callback();
-    });
+    // compiler.plugin("emit", (compilation, callback) => {
+    //   const source = compilation.assets["main.js"].source();
+    //   console.log(source);
+    //   callback();
+    // });
   }
 }
 
