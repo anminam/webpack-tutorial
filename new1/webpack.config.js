e9d8386d734f91ac8bda4d730e7fcd9f6d7e9e8a
anminam <minamhunmin@gmail.com>
2020-11-03 11:17:41 +0900

    update

--- new1/webpack.config.js
+++ new1/webpack.config.js
@@ -1,22 +1,20 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        main: './src/app.js'
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve('./dist')
-    },
-    module: {
-        // rules: [{
-        //     test:/\.js$/,
-        //     use: [path.resolve('./myloader.js')]
-        // }]
-        rules: [{
-            test: /\.css$/,
-            use:["style-loader","css-loader"]
-        }]
-    }
-}
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["css-loader"],
+      },
+    ],
+  },
+};
