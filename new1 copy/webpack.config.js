9d8366b9c36502da6129ade202c776c0a600d427
anminam <minamhunmin@gmail.com>
2020-11-09 09:09:29 +0900

    update

---
+++ new1 copy/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const {
+  CleanWebpackPlugin,
+} = require("../new2/node_modules/clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: `[name].js`,
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: `[name].[ext]?[hash]`,
+              publicPath: "../dist",
+              limit: 10000,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // 템플릿 경로를 지정
+      templateParameters: {
+        // 템플릿에 주입할 파라매터 변수 지정
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "2313",
+      },
+    }),
+  ],
+};
---
+++ new2/index.js
@@ -0,0 +1,4 @@
+const sum = (a, b) => a + b;
+
+const a = sum(a, b);
+console.log(a);
