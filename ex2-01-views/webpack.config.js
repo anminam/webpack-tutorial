51e3a9e03600db1070716acf0ad6b859c17a276f
anminam <minamhunmin@gmail.com>
2020-11-10 17:01:28 +0900

    update config

--- ex2-01-views/webpack.config.js
+++ ex2-01-views/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -8,5 +9,25 @@ module.exports = {
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(c|sc)ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 8888,
+    proxy: {
+      "/api": "http://localhost:8081",
+    },
   },
 };
