ec8b42d630b23e1424c7cbbdfaa82267ba66eba1
anminam <minamhunmin@gmail.com>
2020-11-10 10:07:39 +0900

    add dev  server

--- ex1-04-dev-server/webpack.config.js
+++ ex1-04-dev-server/webpack.config.js
@@ -1,34 +1,26 @@
 const path = require("path");
-const HtmlWebpachPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const apiMocker = require("connect-api-mocker");
+
 module.exports = {
   mode: "development",
   entry: {
-    app: path.resolve(__dirname, "./src/app.js"),
+    app: "./src/app.js",
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("./dist"),
-  },
-  devServer: {
-    before: (app, server, compiler) => {
-      app.get("/api/keywords", (req, res) => {
-        res.json([
-          { keyword: "안미남" },
-          { keyword: "라기면" },
-          { keyword: "삼성윤" },
-          { keyword: "또안미남" },
-        ]);
-      });
-    },
-    contentBase: path.join(__dirname, "dist"),
-    publicPath: "/",
-    overlay: true,
-    stats: "errors-only",
-    historyApiFallback: true,
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new HtmlWebpachPlugin({
-      template: path.resolve(__dirname, "./src/index.html"),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
     }),
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 8080,
+    before: (app, server, compiler) => {
+      app.use(apiMocker("/api", "mocks/api"));
+    },
+  },
 };
